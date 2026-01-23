f1010e10d033eb4695de5b8b70ab499e94e6ff96
c1a3 <154661452+c1a3@users.noreply.github.com>
2026-01-23 10:23:29 +0000

    added rules

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
-
+const blockList = [
+  "*://*.doubleclick.net/*",
+  "*://*.googlesyndication.com/*",
+  "*://*.adnxs.com/*",
+  "*://*.adservice.google.com/*",
+  "*://*.analytics.google.com/*",
+  "*://*.facebook.com/tr/*",
+  "*://*.scorecardresearch.com/*",
+  "*://*.html-load.com/*"
+];
